cd5cdef766d7b5cdb4903a905c92d8d4db5cf4fe
lexstile <alexey.zverkov@mail.ru>
2019-07-24 18:44:16 +0300

    Added class active for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,50 @@ type AppComponentPropsType = {
 class AppComponent extends React.PureComponent<AppComponentPropsType> {
   state = {
     display: 3,
+    btnActive: {
+      one: false,
+      two: false,
+      three: true,
+    },
   }
 
-  oneInRow = () => this.setState({ display: 1 });
+  oneInRow = () => this.setState({
+    display: 1,
+    btnActive: {
+      one: true,
+      two: false,
+      three: false,
+    },
+  });
 
-  twoInRow = () => this.setState({ display: 2 });
+  twoInRow = () => this.setState({
+    display: 2,
+    btnActive: {
+      one: false,
+      two: true,
+      three: false,
+    },
+  });
 
-  threeInRow = () => this.setState({ display: 3 });
+  threeInRow = () => this.setState({
+    display: 3,
+    btnActive: {
+      one: false,
+      two: false,
+      three: true,
+    },
+  });
 
   render() {
     const { movies, recommended } = this.props;
-    const { display } = this.state;
+    const { display, btnActive } = this.state;
     return (
       <React.Fragment>
         <Header>
           React Movie Cards
         </Header>
         <TypeOfDisplay
+          btnActive={btnActive}
           oneInRow={this.oneInRow}
           twoInRow={this.twoInRow}
           threeInRow={this.threeInRow}
--- src/components/TypeOfDisplay/TypeOfDisplay.jsx
+++ src/components/TypeOfDisplay/TypeOfDisplay.jsx
@@ -4,85 +4,39 @@ import './TypeOfDisplay.less';
 
 export class TypeOfDisplay extends React.PureComponent<{}> {
   render() {
-    const { oneInRow, twoInRow, threeInRow } = this.props;
+    const {
+      oneInRow,
+      twoInRow,
+      threeInRow,
+      btnActive,
+    } = this.props;
     return (
       <div className="display">
-        <button type="button" onClick={oneInRow}>
+        <button className={btnActive.one ? 'gold' : 'black'} type="button" onClick={oneInRow}>
           <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 455 455">
-            <g>
-              <rect width="455" height="212" />
-              <rect y="242" width="455" height="213" />
-            </g>
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
+            <rect width="455" height="212" />
+            <rect y="242" width="455" height="213" />
           </svg>
         </button>
-        <button type="button" onClick={twoInRow}>
+        <button className={btnActive.two ? 'gold' : 'black'} type="button" onClick={twoInRow}>
           <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 455 455">
-            <g>
-              <rect width="213" height="212" />
-              <rect x="243" width="212" height="212" />
-              <rect y="242" width="213" height="213" />
-              <rect x="243" y="242" width="212" height="213" />
-            </g>
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
+            <rect width="213" height="212" />
+            <rect x="243" width="212" height="212" />
+            <rect y="242" width="213" height="213" />
+            <rect x="243" y="242" width="212" height="213" />
           </svg>
         </button>
-        <button type="button" onClick={threeInRow}>
+        <button className={btnActive.three ? 'gold' : 'black'} type="button" onClick={threeInRow}>
           <svg version="1.1" id="Layer_3" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 455 455">
-            <g>
-              <rect x="162" y="161" width="132" height="132" />
-              <rect x="162" y="323" width="132" height="132" />
-              <rect y="161" width="132" height="132" />
-              <rect x="324" width="131" height="131" />
-              <rect width="132" height="131" />
-              <rect x="162" width="132" height="131" />
-              <rect x="324" y="323" width="131" height="132" />
-              <rect y="323" width="132" height="132" />
-              <rect x="324" y="161" width="131" height="132" />
-            </g>
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
-            <g />
+            <rect x="162" y="161" width="132" height="132" />
+            <rect x="162" y="323" width="132" height="132" />
+            <rect y="161" width="132" height="132" />
+            <rect x="324" width="131" height="131" />
+            <rect width="132" height="131" />
+            <rect x="162" width="132" height="131" />
+            <rect x="324" y="323" width="131" height="132" />
+            <rect y="323" width="132" height="132" />
+            <rect x="324" y="161" width="131" height="132" />
           </svg>
         </button>
       </div>
